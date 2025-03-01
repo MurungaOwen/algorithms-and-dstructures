45d3aeed2a503436ee73df36f8338ae288e3c72f
(-)00D <122882250+MurungaOwen@users.noreply.github.com>
2025-03-01 19:49:31 +0300

    refactor function to reverse array

--- min_max.py
+++ min_max.py
@@ -22,18 +22,12 @@ def sort_desc(arr):
         index += 1
     return arr
 
-def sort_asc(arr):
-    index = 0
-    while index < len(arr):
-        for i in range(index+1, len(arr)):
-            if arr[i] < arr[index]:
-                arr[i], arr[index] = arr[index], arr[i]
-        index += 1
-    return arr
 def miniMaxSum(arr):
     # Write your code here
+
+    max_arr = sort_desc(arr)
     max_sum = list(accumulate(sort_desc(arr)))[3]
-    min_sum = list(accumulate(sort_asc(arr)))[3]
+    min_sum = list(accumulate(max_arr[::-1]))[3] # just reverse the sorted instead of creating function to sort in asc
     print("{} {}".format(min_sum, max_sum))
 
 if __name__ == '__main__':
